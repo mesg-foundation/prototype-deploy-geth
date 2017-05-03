f26f7f33080cf014c974df1eac003de05e31fe42
Nicolas Mahé <nicolas@mahe.me>
2017-05-03 11:39:17 +0700

    Add dashboard deploy script

---
+++ website/dashboard/build/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if git diff HEAD~ --name-only|grep $DASHBOARD_PATH; then
+  echo "Environment $1"
+
+  #echo "Install yarn"
+  #npm install --global yarn
+
+  echo "Build node modules"
+  cd $DASHBOARD_PATH
+  yarn
+  cd $CI_PROJECT_DIR
+
+  echo "Build dashboard"
+  npm --prefix $DASHBOARD_PATH run build
+
+  echo "Deploying dist folder to s3"
+  aws s3 sync $DASHBOARD_DIST_PATH s3://$DASHBOARD_BUCKET_NAME --recursive --acl public-read
+fi;
