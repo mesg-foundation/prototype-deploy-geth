ebb7ff65df9f7cc52aaac53face74ceb9f2958fa
Nicolas Mahé <nicolas@mahe.me>
2017-05-09 17:42:24 +0700

    update server scripts archive endpoint

--- lambda/digitalocean-create-droplet/handler.js
+++ lambda/digitalocean-create-droplet/handler.js
@@ -9,7 +9,7 @@ var DigitalOcean = new DigitalOceanWrapper(process.env.ES_DIGITALOCEAN_KEY, 25)
  * @return The script
  */
 const userDataScript = (subscriptionId) => {
-  const endpoint = process.env.ES_SERVER_SCRIPT_SETUP_ENDPOINT
+  const endpoint = process.env.ES_SERVER_SCRIPTS_ARCHIVE_ENDPOINT
   const endpoint_droplet_created = process.env.ES_ENDPOINT_DROPLET_CREATED
   const token = process.env.ES_GITLAB_KEY
   return `#!/bin/bash
