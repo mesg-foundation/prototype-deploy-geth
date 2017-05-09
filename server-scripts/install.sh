afeb7cd5c560cae935ded8c2242bf84ed99ed632
Nicolas Mahé <nicolas@mahe.me>
2017-05-09 15:13:47 +0700

    fix server scripts

--- server-scripts/install.sh
+++ server-scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# @todo: ne pas export mais source /root/.bashrc
+# @TODO: ne pas export mais source /root/.bashrc
 
 export ES_PARITY_SHARED_FOLDER=/root/parity
-export ES_PARITY_VERSION=1.6.6
-echo '
+export ES_PARITY_VERSION=v1.6.6
+echo "
 export ES_PARITY_SHARED_FOLDER=$ES_PARITY_SHARED_FOLDER
-export ES_PARITY_VERSION=ES_PARITY_VERSION
-' >> /root/.bashrc
+export ES_PARITY_VERSION=$ES_PARITY_VERSION
+" >> /root/.bashrc
 
 # Create swap
 sh create-swap.sh
--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -4,23 +4,27 @@
 ip=$(hostname  -I | awk -F" " '{print $1}')
 hostname=$(hostname)
 
-# @todo: ne pas export mais source /root/.bashrc
+# @TODO: ne pas export mais source /root/.bashrc
 export ES_STRIPE_SUBSCRIPTION_ID=$1
-echo '
-export ES_STRIPE_SUBSCRIPTION_ID=$ES_STRIPE_SUBSCRIPTION_ID
-' >> /root/.bashrc
-
 export ES_ENDPOINT_DROPLET_CREATED=$2
-echo '
+echo "
+export ES_STRIPE_SUBSCRIPTION_ID=$ES_STRIPE_SUBSCRIPTION_ID
 export ES_ENDPOINT_DROPLET_CREATED=$ES_ENDPOINT_DROPLET_CREATED
-' >> /root/.bashrc
+" >> /root/.bashrc
+
+# Generate the parity signer token
+parity_signer_token=$(sh parity/generate-signer-token.sh)
 
 # Launch parity
-sh parity/run.sh
+#sh parity/run.sh
+sh parity/install.sh
 
 # Execute webhook on Lambda
 curl -X POST \
   $ES_ENDPOINT_DROPLET_CREATED \
-  -F subscription-id=$ES_STRIPE_SUBSCRIPTION_ID \
-  -F ip=$ip \
-  -F servername=$hostname \
+  -d "{
+    \"subscription-id\": \"$ES_STRIPE_SUBSCRIPTION_ID\",
+    \"ip\": \"$ip\",
+    \"servername\": \"$hostname\",
+    \"parity-signer-token\": \"$parity_signer_token\"
+  }"
