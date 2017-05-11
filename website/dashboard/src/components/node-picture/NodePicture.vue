de041632cd379b9741dd2c6ab48e5eb4bf658056
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-05-11 23:03:08 +0700

    add node picture component

---
+++ website/dashboard/src/components/node-picture/NodePicture.vue
@@ -0,0 +1,36 @@
+<script>
+  export default {
+    props: {
+      node: {
+        type: Object,
+        default: {}
+      }
+    },
+    computed: {
+      src () {
+        return require('@/assets/ethereum.png')
+      }
+    },
+    render (createElement) {
+      return createElement('img', {
+        style: this.style,
+        attrs: {
+          src: this.src
+        }
+      })
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../variables'
+
+  img {
+    height: 1.5em;
+    width: 1.5em;
+    border-radius: 50%;
+    float: left;
+    margin-right: 1em;
+  }
+</style>
+
