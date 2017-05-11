68a8ffa0cd9e7a52900eb113ddff5db14ffd3f07
Nicolas Mahé <nicolas@mahe.me>
2017-05-11 14:41:41 +0700

    add instance name

--- server-scripts/parity/install.sh
+++ server-scripts/parity/install.sh
@@ -5,6 +5,7 @@ envPath=/root/server-scripts/.es.env
 echo "
 export ES_PARITY_SHARED_FOLDER=/root/parity
 export ES_PARITY_VERSION=v1.6.6
+export ES_PARITY_DOCKER_NAME=parity
 " >> $envPath
 source $envPath
 
