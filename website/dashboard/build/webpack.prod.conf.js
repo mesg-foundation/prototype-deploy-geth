55eb6afc4a094d5dc981ca04759f0e1f91584e8f
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-05-11 17:33:46 +0700

    add possibility to pass a config file for build

--- website/dashboard/build/webpack.prod.conf.js
+++ website/dashboard/build/webpack.prod.conf.js
@@ -9,9 +9,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
-var env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : config.build.env
+var env = config.build.env
+if (process.env.NODE_ENV === 'testing') { env = require('../config/test.env') }
+if (process.argv[2]) { env = require('../' + process.argv[2]) }
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
