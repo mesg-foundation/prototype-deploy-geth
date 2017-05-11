4292d1ee4078996b15f02aeedbed261efc61b75f
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-05-11 17:53:23 +0700

    add debug info and fix id vs uid from firebase

--- lambda/stripe-create-customer/handler.js
+++ lambda/stripe-create-customer/handler.js
@@ -40,7 +40,7 @@ const createCustomer = data => {
     source: data.stripeToken,
     email: data.stripeEmail,
     metadata: {
-      firebase_id: data.user.id
+      firebase_id: data.user.uid
     }
   };
   return stripe.customers.create(customerData)
@@ -69,6 +69,7 @@ const createSubscription = (data, customer) => {
  */
 module.exports.createCustomer = (event, context, callback) => {
   const data = JSON.parse(event.body);
+  console.log(JSON.stringify(data))
   createCustomer(event)
   .then(customer => createSubscription(event, customer))
   .then(subscription => success(subscription, callback))
