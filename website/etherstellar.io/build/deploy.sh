bd40a1117d23e439d913c2ce330c6c57690e5c64
Nicolas Mahé <nicolas@mahe.me>
2017-05-20 11:12:32 +0700

    create new simple website

--- website/etherstellar.io/build/deploy.sh
+++ website/etherstellar.io/build/deploy.sh
@@ -3,14 +3,6 @@
 #if git diff HEAD~ --name-only|grep $ETHERSTELLAR_IO_PATH; then
   echo "## Environment $1 ##"
 
-  echo "## Build node modules ##"
-  cd $ETHERSTELLAR_IO_PATH
-  yarn
-  cd $CI_PROJECT_DIR
-
-  echo "## Build dashboard ##"
-  npm --prefix $ETHERSTELLAR_IO_PATH run build
-
   echo "## Deploying dist folder to s3 ##"
   aws s3 sync $ETHERSTELLAR_IO_DIST_PATH s3://$ETHERSTELLAR_IO_BUCKET_NAME --acl public-read
 #fi;
